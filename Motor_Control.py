99c7bc9ce5ce2a83214845a3f66ade001c1edb43
nahyland <43459066+nahyland@users.noreply.github.com>
2018-09-27 13:15:23 -0700

    Create Motor_Control.py

---
+++ Motor_Control.py
@@ -0,0 +1,14 @@
+### Code for controlling a DC motor with an H-bridge ###
+
+
+## Import Libraries ##
+import time
+import RPi.GPIO as GPIO
+import SMBus
+
+## Assigning I2C bus addresses ##
+
+
+## Defining functions ##
+def read_bd:
+	return bus.read_byte_data(address, adr)
