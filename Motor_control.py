c743db3d4d12f71287da12bbee4b0bcaa3f08bcc
nahyland <nahyland@users.noreply.github.com>
2018-10-01 15:32:32 -0700

    Motor_control.py now updates with push

--- Motor_control.py
+++ Motor_control.py
@@ -1,29 +1,60 @@
-
+# Import Libraries#
 import time
-import RPi.GPIO as GPIO
+import RPi.GPIO as gpio
 
-In1 = 8
-In2 = 10
+# Define Vairables #
+Out1 = 23
+Out2 = 24
+freq = 500
+dc_start = 5
 
-def init():
-	GPIO.setmode(GPIO.BOARD)
-	GPIO.setup(In1, GPIO.OUT)
-	GPIO.setup(In2, GPIO.OUT)
+
+#def init():
+gpio.setmode(gpio.BCM)
+gpio.setup(Out1, gpio.OUT)
+gpio.setup(Out2, gpio.OUT)
+
+pwm1 = gpio.PWM(Out1, freq)
+pwm2 = gpio.PWM(Out2, freq)
 
 def cw(t):
-	init()
-	GPIO.output(In1, GPIO.HIGH)
-	GPIO.output(In2, GPIO.LOW)
-	time.sleep(t)
-	GPIO.cleanup()
+#	init()
+	gpio.output(Out1, gpio.HIGH)
+#	gpio.output(Out2, gpio.LOW)
+	time.sleep(1)
+	gpio.output(Out1, gpio.LOW)
+	time.sleep(1)
+	rampit(t)
+#	time.sleep(t)
+	gpio.cleanup()
 
 def ccw(t):
-	init()
-	GPIO.output(In1, GPIO.LOW)
-	GPIO.output(In2, GPIO.HIGH)
-	time.sleep(t)
-	GPIO.cleanup()
+#	init()
+#	gpio.output(Out1, gpio.Low)
+#	gpio.output(Out2, gpio.HIGH)
+#	rampit(t, divs)
+#	time.sleep(t)
+	gpio.cleanup()
+
+def rampit(t):
+	pauseme =t / 10
+	pwm1.start(5)
+	time.sleep(0.5)
+	sickle = dc_start
+	for i in range (0, 9):
+		time.sleep(pauseme)
+		sickle += 10
+		pwm1.ChangeDutyCycle(sickle)
+		print("Duty Cycle:", sickle)
+	for i in range (0, 9):
+		time.sleep(pauseme)
+		sickle -= 10
+		pwm1.ChangeDutyCycle(sickle)
+		print("Duty Cycle:", sickle)
 
 cw(4)
 
-ccw(4)
+#ccw(10)
+
+
+
