b6d0afb02901e4a4efcf844a1fe61769c6616d4f
nahyland <43459066+nahyland@users.noreply.github.com>
2018-10-28 13:11:23 -0700

    Update Robot/PID/PID.py
    Added setup, import, and initialization
    wrote basics of class code
    wrote overall guidelines in

--- Robot/PID/PID.py
+++ Robot/PID/PID.py
@@ -1,10 +1,59 @@
 # ME 5731 Project 2: Part 3
 # Author: Harrison Seung and Nicholas Hyland
 # Group: 9
-# Description: Create a code to control the provided robot with 8 outputs and 4 inputs
+# Description: Create a code to control the provided robot position with a PID controller(digital)
 
-## Initial Setup ##
-# Import libraries
-import time
+
+#- Initialize -#
 import RPi.GPIO as gpio
-import smbus
+import os, time, math, smbus
+
+# ADS1115
+from ADS1115 import ADS1115
+from ADS1115.ADS1115 import ADS_CONFIG_MUX
+from ADS1115.ADS1115 import ADS_CONFIG_GAIN
+# PCA9685
+from PCA9685 import PCA9685
+# rbotInit
+from Initialization import rbotInit
+# PosRead
+from PosRead import PosRead
+
+# Setup Variables/Arrays #
+ads = ADS1115.ADS1115()
+pca = PCA9685.PCA9685()
+rbot = rbotInit.rbotInit()
+pread = PosRead.PosRead()
+
+ptstore = [0,0,0:0,0,0;0,0,0;0,0,0]
+#- End Initialize -#
+
+#- Class -#
+class PID(object):
+	def pidcontrol(leddir, poslimit[], rbotwant):		# [poslimit contains the ranges of motion available
+		# Storing old values for Integrator and Differentiator
+		for i in range (0,4):
+			for k in range (90, 3):
+				ptstore[i, k+2] = ptstore[i, k+1]
+				ptstore[i, k+1] = ptstore[i, k]
+			startpos[i] = pread.rbotpos()
+		# Proportional
+		rbotdist = (rbotwant - rbotposit[motor])/(poslimit[2]-poslimit[1])
+		pmove = ?
+
+		# Integral, Simpson's 3/8 rule
+		for i in range (0, 4):
+			h = tcurr-tstart
+			intgl = h/3*(startpos[i])
+
+
+
+
+		# Derivative
+		use 3 points to determine slope
+		for i in range (0, 4):
+			dslope = (ptstore[i, 2]+ptstore[i, 1]+ptstore[i, 0])/3
+
+
+
+		# Combining terms
