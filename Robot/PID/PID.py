2071418f81944456a8e453d867d12df08215a963
nahyland <43459066+nahyland@users.noreply.github.com>
2018-10-31 10:59:57 -0700

    Update Robot/PID/PID.py
    Added running loop
    Added loop counter (for integral)
    Added matrices as needed
    Finished math in Integral section

--- Robot/PID/PID.py
+++ Robot/PID/PID.py
@@ -19,41 +19,65 @@ from Initialization import rbotInit
 # PosRead
 from PosRead import PosRead
 
-# Setup Variables/Arrays #
+# Setup Variables/Matrices #
+# Variables
 ads = ADS1115.ADS1115()
 pca = PCA9685.PCA9685()
 rbot = rbotInit.rbotInit()
 pread = PosRead.PosRead()
 
+# Matrices(lists)
 ptstore = [0,0,0:0,0,0;0,0,0;0,0,0]
+loopcount = 0
+sumval = [0,0,0,0]
+intgl = [0,0,0,0]
+dslope = [0,0,0,0]
 #- End Initialize -#
 
 #- Class -#
 class PID(object):
-	def pidcontrol(leddir, poslimit[], rbotwant):		# [poslimit contains the ranges of motion available
-		# Storing old values for Integrator and Differentiator
-		for i in range (0,4):
-			for k in range (90, 3):
-				ptstore[i, k+2] = ptstore[i, k+1]
-				ptstore[i, k+1] = ptstore[i, k]
-			startpos[i] = pread.rbotpos()
-		# Proportional
-		rbotdist = (rbotwant - rbotposit[motor])/(poslimit[2]-poslimit[1])
-		pmove = ?
+	def pidcontrol(leddir, rbotwant, holdt):		#  holdt is the time to hold end position *500
+		# Loop continues until all positions are at rbotwant positions
+		while ptstore[i, 0]!=rbotwant[0] & ptstore[i, 1]!=rbotwant[1] & ptstore[i, 2]!=rbotwant[2] & ptstore[i, 3]!=rbotwant[3]
+			loopcount += 1
+			# Storing old values for Integrator and Differentiator, 0 is current, 1 is last, 2 is oldest
+			for i in range (0,4):
+				for k in range (0, 3):
+					ptstore[i, k+2] = ptstore[i, k+1]
+					ptstore[i, k+1] = ptstore[i, k]
+					# Record new position values
+					ptstore[i] = pread.rbotpos()
 
-		# Integral, Simpson's 3/8 rule
-		for i in range (0, 4):
-			h = tcurr-tstart
-			intgl = h/3*(startpos[i])
+					# Record initial position on first loop
+					if loopcount = 1:
+						startpos[i] = pread.rbotpos()
+					else:
+
+					# Proportional
+					rbotdist = (rbotwant - rbotposit[motor])/(poslimit[2]-poslimit[1])
+					pmove = ?
+
+					# Integral, Simpson's Composite Rule
+					if loopcount > 2
+					h = tcurr-tstart / loopcount
+					for i in range (0, 4):
+						sumval[i] += 3*ptstore[i, 1] +ptstore[i, 0]
+						intgl[i] = h/3*(sumval)
+					else:
+						sumval[i] += ptstore[i, 0]
 
 
 
 
-		# Derivative
-		use 3 points to determine slope
-		for i in range (0, 4):
-			dslope = (ptstore[i, 2]+ptstore[i, 1]+ptstore[i, 0])/3
+					# Derivative
+					use 3 points to determine slope
+					for i in range (0, 4):
+						dslope[i] = (ptstore[i, 2]+ptstore[i, 1]+ptstore[i, 0])/3
 
 
 
 		# Combining terms
+
+
+
+#- End Class -#
