c93c45212180e2964374eef8bb5a7fb4f9083dc1
nahyland <43459066+nahyland@users.noreply.github.com>
2018-11-14 16:21:21 -0800

    Update Robot/Waypoints/Waypoints.py
    Added file to reach in waypoints

---
+++ Robot/Waypoints/Waypoints.py
@@ -0,0 +1,22 @@
+# ME 5731 Project 2: Part 1
+# Author: Harrison Seung and Nicholas Hyland
+# Group: 9
+# Description: File for setting waypoints
+
+# Waypoints #
+# wp = [azimuth, vertical, arm]; measured in degrees and inches
+wp1 = [0,0,0]
+wp2 = [0,0,0]
+wp3 = [0,0,0]
+wp4 = [0,0,0]
+wp5 = [0,0,0]
+wp6 = [0,0,0]
+wp7 = [0,0,0]
+
+
+
+#- Class -#
+Class Waypoints(self):
+	def waypoint(self):
+		waypt = [wp1;wp2;wp3;wp4;wp5;wp6;wp7]
+	return waypt
