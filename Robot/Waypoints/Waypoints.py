149067f5edab17ebac42a045486b3ce8f4333657
nahyland <43459066+nahyland@users.noreply.github.com>
2018-11-15 10:45:28 -0800

    Update Waypoints.py
    simplified waypoint setting, now based on starting position instead of manually

--- Robot/Waypoints/Waypoints.py
+++ Robot/Waypoints/Waypoints.py
@@ -4,19 +4,28 @@
 # Description: File for setting waypoints
 
 # Waypoints #
-# wp = [azimuth, vertical, arm]; measured in degrees and inches
-wp1 = [0,0,0]
-wp2 = [0,0,0]
-wp3 = [0,0,0]
-wp4 = [0,0,0]
-wp5 = [0,0,0]
-wp6 = [0,0,0]
-wp7 = [0,0,0]
+beginpos = [0,0,0]
+cuppos = [20,5,5]
+droppos = [140,8,7]
 
+# wp = [azimuth, vertical, arm]; measured in degrees and inches
+wp1 = [beginpos[0],beginpos[1],beginpos[2]]
+wp2 = [cuppos[0],cuppos[1]-1,cuppos[2]-2]
+wp3 = [cuppos[0],cuppos[1]-1,cuppos[2]]
+wp4 = [cuppos[0],cuppos[1]+1,cuppos[2]]
+wp5 = [droppos[0],droppos[1]+1,droppos[2]]
+wp6 = [droppos[0],droppos[1]-1,droppos[2]]
+wp7 = [droppos[0],droppos[1]-1,droppos[2]-2]
+wp8 = [beginpos[0],beginpos[1],beginpos[2]]
+
+# DC for speed
+dcwant = 50
 
 
 #- Class -#
 Class Waypoints(self):
 	def waypoint(self):
-		waypt = [wp1;wp2;wp3;wp4;wp5;wp6;wp7]
+		waypt = [wp1;wp2;wp3;wp4;wp5;wp6;wp7;wp8]
 	return waypt
+	return dcwant
+#- End Class -#
